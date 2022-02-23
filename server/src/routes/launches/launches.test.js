01aa5e9c518cde4cd1160ca4209844f08c1697e2
Your Name <you@example.com>
2022-02-23 13:48:23 +0100

    Load planets data in test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../../app');
 const { mongoConnect, mongoDisconnect } = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 
 describe('TEST API CALLS', () => {
@@ -8,6 +9,7 @@ describe('TEST API CALLS', () => {
     beforeAll(async () => {
     
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async () => {
